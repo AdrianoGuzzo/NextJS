95d04125d78bd4e631bfa705e600c4740f6866d0
Adriano Guzzo Rosa Bastos <adriano.guzzo@smait.com.br>
2020-11-11 21:24:54 -0300

    Teste do contador

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,19 @@
+import React, { useState } from "react";
+
 function Home() {
-  return <div>oi teste</div>;
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <h1>Site teste</h1>
+      <p>Contador {count}</p>
+      <button
+        onClick={() => {
+          setCount((count) => (count += 1));
+        }}
+      >
+        Click Aqui
+      </button>
+    </>
+  );
 }
 export default Home;
