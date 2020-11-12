ea22bc93602874b114d1b5410807f7e5c6072bbf
Adriano Guzzo Rosa Bastos <adriano.guzzo@smait.com.br>
2020-11-11 21:38:03 -0300

    Teste favicon

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
+import Head from "next/head";
 
 function Home() {
   const [count, setCount] = useState(0);
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <h1>Site teste</h1>
       <p>Contador {count}</p>
       <button
